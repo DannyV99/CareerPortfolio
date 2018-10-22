1c48c1472fadf405a4da3eb3e057e509baf8863c
DannyV99 <dgvonmoser@gmail.com>
2018-10-22 10:37:57 -0500

    Initial Submission

---
+++ main.js
@@ -0,0 +1,17 @@
+var specificPropertyParameters = anime({
+    targets: '#specificPropertyParameters .el',
+    scale: {
+        value: 1.4,
+        duration: 1900,
+        delay: 800,
+        easing: 'easeInOutQuart',
+    },
+    delay: 250 // All properties except 'scale' inherit 250ms delay
+});
+
+function onHover() {
+    $("#menuImg").attr('src', 'images/daniel2.jpg');
+}
+function offHover() {
+    $("#menuImg").attr('src', 'images/daniel1.jpg');
+}
